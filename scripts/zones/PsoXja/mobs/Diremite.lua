f62bd07361ca71c0c641ecc7037a1a1662de24a4
Brian Silvi <38861984+Lynnea1320@users.noreply.github.com>
2020-06-25 18:15:49 -0400

    Update Diremite.lua
    Removed semicolons

--- scripts/zones/PsoXja/mobs/Diremite.lua
+++ scripts/zones/PsoXja/mobs/Diremite.lua
@@ -2,11 +2,11 @@
 -- Area: Pso'Xja
 --  Mob: Diremite
 -----------------------------------
-local ID = require("scripts/zones/PsoXja/IDs");
+local ID = require("scripts/zones/PsoXja/IDs")
 
 function onMobDeath(mob, player, isKiller)
-end;
+end
 
 function onMobDespawn(mob)
     tpz.mob.phOnDespawn(mob, ID.mob.GYRE_CARLIN_PH, 5, 1800) -- 30 minutes.
-end;
+end
